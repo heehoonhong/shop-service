d4bdb6b9dd6fc2c440ac4e2a06e9b664fac3c4a2
heehoonhong <gmlgns0522@naver.com>
2025-01-08 20:38:14 +0900

    last commit

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,12 +3,12 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
-import java.util.function.Predicate;
 
 @Repository
 @RequiredArgsConstructor
@@ -71,4 +71,48 @@ public class OrderRepository {
     }
 
 
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m"+
+                        " join fetch o.delivery d",Order.class).getResultList();
+
+    }
+
+
+
+
+
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+        return em.createQuery(
+                "select o from Order o"+
+                        " join o.member m"+
+                        " join o.delivery d",OrderSimpleQueryDto.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o"+
+                        " join fetch o.member m"+
+                        " join fetch o.delivery d"+
+                        " join fetch o.orderItems oi"+
+                        " join fetch oi.item i",Order.class)
+                .getResultList();
+
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m"+
+                        " join fetch o.delivery d",Order.class).setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+
+
+
+
+    }
 }
